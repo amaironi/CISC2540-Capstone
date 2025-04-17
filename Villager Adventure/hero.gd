2018e70b4a764b97cd3ef218236959841f247d11
Anton M <antonmaironi@gmail.com>
2025-04-17 01:02:42 -0400

    Looped Hero start jump behavior
    Made jumping logic loop and stop when the player tries to "interact" with the Hero

--- Villager Adventure/hero.gd
+++ Villager Adventure/hero.gd
@@ -3,6 +3,8 @@ extends Node3D
 var jumping = false
 var landing = false
 var slide = false
+var keep_jumping = true
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,13 +13,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if jumping == true:
+	if jumping == true and keep_jumping == true:
 		position.y += 0.2*delta
 		position.x += 1.9*delta
-	elif jumping == false and landing == true:
+	elif jumping == false and landing == true and keep_jumping == true:
 		position.x += 1.5*delta
 		position.y -= 0.8*delta
-	if slide == true:
+	if slide == true and keep_jumping == true:
 		position.x+= 1.2*delta
 
 
@@ -27,16 +29,31 @@ func _on_timer_timeout() -> void:
 	jumping = true
 
 func _on_jump_timer_timeout() -> void:
-	$ANIMATIONS.play("Jump") 
-	$ANIMATIONS/no_jump_timer.start()
-	jumping = false
-	landing = true
+	if keep_jumping == true:
+		$ANIMATIONS.play("Jump") 
+		$ANIMATIONS/no_jump_timer.start()
+		jumping = false
+		landing = true
+		$ANIMATIONS/restart_jump.start()
 
 func _on_no_jump_timer_timeout() -> void:
-	$ANIMATIONS.play("Jump_Land")
-	landing = false
-	slide = true
-	$ANIMATIONS/slide.start()
+	if keep_jumping == true:
+		$ANIMATIONS.play("Jump_Land")
+		landing = false
+		slide = true
+		$ANIMATIONS/slide.start()
 
 func _on_slide_timeout() -> void:
-	slide = false
+	if keep_jumping == true:
+		slide = false
+
+
+func _on_restart_jump_timeout() -> void:
+	if keep_jumping == true:
+		$ANIMATIONS.play("Jump_Start")
+		$ANIMATIONS/jump_timer.start()
+		jumping = true
+
+func _on_trigger_start_body_entered(body: Node3D) -> void:
+	$ANIMATIONS.play("Idle_Talking")
+	keep_jumping = false
