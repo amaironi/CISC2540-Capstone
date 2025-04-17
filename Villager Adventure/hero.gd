c8889730dddcb086345d28f890db7fc7db0e8a37
Anton M <antonmaironi@gmail.com>
2025-04-17 17:52:28 -0400

    Finished idle jumping animation at beginning

--- Villager Adventure/hero.gd
+++ Villager Adventure/hero.gd
@@ -4,6 +4,8 @@ var jumping = false
 var landing = false
 var slide = false
 var keep_jumping = true
+var jump_count = 0
+var can_rotate = true
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,14 +15,30 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if jumping == true and keep_jumping == true:
-		position.y += 0.2*delta
-		position.x += 1.9*delta
-	elif jumping == false and landing == true and keep_jumping == true:
-		position.x += 1.5*delta
-		position.y -= 0.8*delta
-	if slide == true and keep_jumping == true:
-		position.x+= 1.2*delta
+	if jump_count <= 3:
+		if jumping == true and keep_jumping == true:
+			position.y += 0.24*delta
+			position.x += 1.9*delta
+		elif jumping == false and landing == true and keep_jumping == true:
+			position.x += 1.5*delta
+			position.y -= 0.7*delta
+		if slide == true and keep_jumping == true:
+			position.x += 1.2*delta
+	
+	if jump_count >= 4:
+		if jumping == true and keep_jumping == true:
+			position.y += 0.24*delta
+			position.x -= 1.9*delta
+		elif jumping == false and landing == true and keep_jumping == true:
+			position.x -= 1.5*delta
+			position.y -= 0.7*delta
+		if slide == true and keep_jumping == true:
+			position.x -= 1.2*delta
+		
+		if jump_count == 4 and can_rotate == true:
+			$turn_around.play("rotate_first")
+			can_rotate = false
+		
 
 
 func _on_timer_timeout() -> void:
@@ -42,6 +60,11 @@ func _on_no_jump_timer_timeout() -> void:
 		landing = false
 		slide = true
 		$ANIMATIONS/slide.start()
+		jump_count += 1
+		if jump_count == 8:
+			jump_count = 0
+			can_rotate = true
+			$turn_around.play("rotate_back")
 
 func _on_slide_timeout() -> void:
 	if keep_jumping == true:
