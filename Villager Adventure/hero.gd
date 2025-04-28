d849cfdb256fbcc54bce26afcc86a7b94fe7812f
Anton M <antonmaironi@gmail.com>
2025-04-28 18:44:32 -0400

    Progressed on level 1 (only end remains)

--- Villager Adventure/hero.gd
+++ Villager Adventure/hero.gd
@@ -8,6 +8,11 @@ var jump_count = 0
 var can_rotate = true
 var look_at_player = false
 var move_to_player = false
+var only_once = true
+var just_once_2 = true
+var to_pos = false
+var play_sigh = false
+var pickable_sword = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -61,8 +66,36 @@ func _process(delta: float) -> void:
 			$AnimationPlayer.stop()
 			$AnimationPlayer.play("Idle")  # Play Idle animation when close enough
 		
-
-
+	if Input.is_action_just_pressed("AdvanceDialogue"):
+		if just_once_2 == true:
+			$"../Trigger_start/Dialogue1/Fade".play("Fadeout")
+			just_once_2 = false
+			$skip.play()
+			$whathappened/lookaround.start()
+			
+	if to_pos == true:
+		$AnimationPlayer.play("Running")
+		var go_to_pos = $AnimationPlayer2/wasdpos.position - position
+		go_to_pos.y = 0
+		var distance = go_to_pos.length()
+			
+		if distance > 0.1:
+			var direction = go_to_pos.normalized()
+			position += direction * 1.6 * delta
+		else:
+			to_pos = false
+			$AnimationPlayer2.play("wasding")
+			$AnimationPlayer2/sigh.play()
+			play_sigh = true
+			
+		
+		if play_sigh == true and $AnimationPlayer2/sigh.playing == false:
+			$AnimationPlayer2/sigh.play()
+			$"../chocolate/AnimationPlayer".play("fade")
+	
+		if pickable_sword == true and Input.is_action_just_pressed("Pickup"):
+			$"../FindableSword".queue_free()
+			#GIVE SWORD TO PLAYER
 
 func _on_timer_timeout() -> void:
 	$AnimationPlayer.play("Jumping")
@@ -86,7 +119,6 @@ func _on_jump_again_timeout() -> void:
 
 
 func _on_trigger_start_body_entered(body: Node3D) -> void:
-		var only_once = true
 		if only_once == true:
 			$AnimationPlayer.stop()
 			$AnimationPlayer.play("Running")
@@ -103,9 +135,30 @@ func _on_trigger_start_body_entered(body: Node3D) -> void:
 			$"../CharacterBody3D".velocity.z = 0
 			$"../Trigger_start/Dialogue1/Input_timer".start()
 			$"../FindableSword/SpotLight3D".visible = true
+			$whathappened.play()
 			
 			
 
 func _on_input_timer_timeout() -> void:
 	$"../CharacterBody3D".can_input = true
 	
+
+
+func _on_lookaround_timeout() -> void:
+	$lookaround.play()
+	$lookaround/gain_control.start()
+
+
+func _on_gain_control_timeout() -> void:
+	look_at_player = false
+	move_to_player = false
+	to_pos = true
+	
+		
+
+var just_once_3 = true
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if just_once_3 == true:
+		just_once_3 = false
+		$"../FindableSword/Area3D/Label".visible = true
+		pickable_sword = true
