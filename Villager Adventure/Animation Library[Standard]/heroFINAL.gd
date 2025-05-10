9bd3db0974b4d01c3629d88ccf71a1f1bfd563d5
Anton M <antonmaironi@gmail.com>
2025-05-10 21:25:33 +0200

    Finishing up last level

--- Villager Adventure/Animation Library[Standard]/heroFINAL.gd
+++ Villager Adventure/Animation Library[Standard]/heroFINAL.gd
@@ -1,8 +1,11 @@
 extends Node3D
 
 var frozen = true
-var stop_distance = 5
+var stop_distance = 6
 var speed = 2
+var just_once = true
+var attack = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,18 +13,23 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var villain = $"../Villain/Node"
+	var villain = $"../Villain"
 	var target_position = Vector3(villain.position.x, position.y, villain.position.z)
-		
-	if frozen == false:
+		#
+	if frozen == false and attack == true:
 		var to_villain = target_position - position
 		var distance = to_villain.length()
 		look_at(Vector3(villain.position.x,2.6,villain.position.z),Vector3.UP,true)
-
+#
 		if distance > stop_distance:
 			var direction = to_villain.normalized()
-			position -= direction * speed * delta
+			position += direction * speed * delta
+			$AnimationPlayer.play("Running")
+
 			#$"../Hero".hit_back = false
 		else:
-			$AnimationPlayer.play("Striking")
+			if just_once == true:
+				$AnimationPlayer.play("Striking")
+				$"../losehptimer".start()
+				just_once = false
 			#$"../Hero".hit_back = true
--- Villager Adventure/villainFINAL.gd
+++ Villager Adventure/villainFINAL.gd
@@ -1,13 +1,11 @@
 extends Node3D
 
 var frozen = true
-var stop_distance = 5
+var stop_distance = 10
 var speed = 3
 var just_once = true
 var new_pos
 var whirled = false
-var hp = 200
-var just_once_2 = true 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -15,7 +13,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var hero = $"../Hero"
+	var hero = $Hero
 	var target_position = Vector3(hero.position.x, position.y, hero.position.z)
 		
 	if frozen == false:
@@ -23,30 +21,20 @@ func _process(delta: float) -> void:
 		var distance = to_hero.length()
 		look_at(Vector3($"../Hero".position.x,2.6,$"../Hero".position.z),Vector3.UP,true)
 
-		if distance > stop_distance:
-			$AnimationPlayer.play("Running")
+		if distance > stop_distance+7:
 			var direction = to_hero.normalized()
 			position += direction * speed * delta
 			#$"../Hero".hit_back = false
 		else:
+			if just_once == true:
+				$AnimationPlayer.play("Whirl")
+				just_once = false
 				#$"../Hero".hit_back = true
+			elif $AnimationPlayer.current_animation != "Whirl" and whirled == true:
 				$AnimationPlayer.play("Strike")
 	
-	if hp <= 0:
-		visible = false
-		$Bosshp.text = "EVILIUS, THE STEALER OF CHOCOLATE: DEFEATED"
-		$"../Hero".attack = false
-		$"../questcomplete".visible = true
-		if just_once_2 == true:
-			$"../questcomplete/AnimationPlayer".play("fade")
-			$"../QUESTS/CheckBox".button_pressed = true
-			$"../QUESTS/Node2D/SparklerQuest".play("Sparkle")
-			just_once_2 = false
-			$"../CSGBox3D/CSGBox3D7/CSGBox3D6/WallUnevenBrickDoorRound/StaticBody3D".queue_free()
-
-	else:
-		$Bosshp.text = "EVILIUS, THE STEALER OF CHOCOLATE: " + str(hp)
-
-func _on_losehptimer_timeout() -> void:
-	hp -= 15
-	$"../Hero".just_once = true
+			if $AnimationPlayer.current_animation == "Whirl":
+				whirled = true
+			if $AnimationPlayer.current_animation != "Whirl" and whirled == true:
+				position.x = 1.7
+				position.z = 5
